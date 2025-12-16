b68aa14ea5bc44cd40c60ec1f1c76ef31a09425a
Vishal Agarwal <vishal.agarwal656@gmail.com>
2025-12-16 18:20:42 +0530

    done some changes in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React it is a interesting Programming
+          Learn React
         </a>
       </header>
     </div>
